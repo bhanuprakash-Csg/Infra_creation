58366d5838b8f046a04af62a19a13cf710074d43
bhanuprakash-Csg <121594907+bhanuprakash-Csg@users.noreply.github.com>
2025-02-27 01:10:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources_subnet"
   location = "West Europe"
